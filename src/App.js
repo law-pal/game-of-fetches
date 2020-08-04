564e0f0c685de7ae9efe5321c028c032da620a79
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-04 14:11:15 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      one: 'http://anapioficeandfire.com/api/characters/16',
+      two: 'http://www.anapioficeandfire.com/api/houses/378 ',
+      three: ' http://www.anapioficeandfire.com/api/houses/229',
+      four: 'http://www.anapioficeandfire.com/api/houses/17',
+      five : 'http://www.anapioficeandfire.com/api/characters/901',
+      six:'http://www.anapioficeandfire.com/api/houses/362',
+      seven: 'http://www.anapioficeandfire.com/api/characters/232'
+    }
+
+    // requestOne = axios.get(one);
+    // requestTwo = axios.get(two);
+    // requestThree = axios.get(three);
+    // requestFour = axios.get(four);
+    // requestFive = axios.get(five);
+    // requestSix = axios.get(six);
+    // requestSeven = axios.get(seven);
+ 
+    // componentDidMount() {
+    //   axios.all([requestOne, requestTwo, requestThree, requestFour,requestFive, requestSix, requestSeven])
+    //   .then(axios.spread((...responses) => {
+    //     const responseOne = responses[0]
+    //     const responseTwo = responses[1]
+    //     const responseThree = responses[2]
+    //     const responseFour = responses[3]
+    //     const responseFive = responses[4]
+    //     const responseSix = responses[5]
+    //     const responseSeven = responses[6]
+    //   }))
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    // }
+
+  }
+
+  requestOne = axios.get(one);
+  requestTwo = axios.get(two);
+  requestThree = axios.get(three);
+  requestFour = axios.get(four);
+  requestFive = axios.get(five);
+  requestSix = axios.get(six);
+  requestSeven = axios.get(seven);
+
+  componentDidMount() {
+    axios.all([requestOne, requestTwo, requestThree, requestFour,requestFive, requestSix, requestSeven])
+    .then(axios.spread((...responses) => {
+      const responseOne = responses[0]
+      const responseTwo = responses[1]
+      const responseThree = responses[2]
+      const responseFour = responses[3]
+      const responseFive = responses[4]
+      const responseSix = responses[5]
+      const responseSeven = responses[6]
+    }))
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+    render() {
+      return(
+          <div>
+
+          </div>
+    );
+  }
 }
+
+
 
 export default App;
