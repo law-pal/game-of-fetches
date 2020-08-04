7776f34287460bf92a33912f66c019fa90f7d6ea
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-08-04 18:44:26 -0500

    third update

--- src/App.js
+++ src/App.js
@@ -2,73 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
+
 class App extends Component {
-  constructor() {
+    constructor() {
     super()
     this.state = {
-      one: 'http://anapioficeandfire.com/api/characters/16',
-      two: 'http://www.anapioficeandfire.com/api/houses/378 ',
-      three: ' http://www.anapioficeandfire.com/api/houses/229',
-      four: 'http://www.anapioficeandfire.com/api/houses/17',
-      five : 'http://www.anapioficeandfire.com/api/characters/901',
-      six:'http://www.anapioficeandfire.com/api/houses/362',
-      seven: 'http://www.anapioficeandfire.com/api/characters/232'
-    }
-
-    // requestOne = axios.get(one);
-    // requestTwo = axios.get(two);
-    // requestThree = axios.get(three);
-    // requestFour = axios.get(four);
-    // requestFive = axios.get(five);
-    // requestSix = axios.get(six);
-    // requestSeven = axios.get(seven);
- 
-    // componentDidMount() {
-    //   axios.all([requestOne, requestTwo, requestThree, requestFour,requestFive, requestSix, requestSeven])
-    //   .then(axios.spread((...responses) => {
-    //     const responseOne = responses[0]
-    //     const responseTwo = responses[1]
-    //     const responseThree = responses[2]
-    //     const responseFour = responses[3]
-    //     const responseFive = responses[4]
-    //     const responseSix = responses[5]
-    //     const responseSeven = responses[6]
-    //   }))
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
-    // }
-
+    data: [],
   }
+}
 
-  requestOne = axios.get(one);
-  requestTwo = axios.get(two);
-  requestThree = axios.get(three);
-  requestFour = axios.get(four);
-  requestFive = axios.get(five);
-  requestSix = axios.get(six);
-  requestSeven = axios.get(seven);
+componentDidMount() {
+let one = 'http://anapioficeandfire.com/api/characters/16'
+let two = 'http://www.anapioficeandfire.com/api/houses/378 '
+let three = ' http://www.anapioficeandfire.com/api/houses/229'
+let four = 'http://www.anapioficeandfire.com/api/houses/17'
+let five =  'http://www.anapioficeandfire.com/api/characters/901'
+let six = 'http://www.anapioficeandfire.com/api/houses/362'
+let seven = 'http://www.anapioficeandfire.com/api/characters/232'
 
-  componentDidMount() {
-    axios.all([requestOne, requestTwo, requestThree, requestFour,requestFive, requestSix, requestSeven])
-    .then(axios.spread((...responses) => {
-      const responseOne = responses[0]
-      const responseTwo = responses[1]
-      const responseThree = responses[2]
-      const responseFour = responses[3]
-      const responseFive = responses[4]
-      const responseSix = responses[5]
-      const responseSeven = responses[6]
-    }))
-    .catch(error => {
-      console.log(error);
-    });
-  }
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
+const requestFour = axios.get(four);
+const requestFive = axios.get(five);
+const requestSix = axios.get(six);
+const requestSeven = axios.get(seven);
 
-    render() {
+  axios.all([requestOne, requestTwo, requestThree, requestFour,requestFive, requestSix, requestSeven])
+  .then(axios.spread((...responses) => {
+    const responseOne = responses[0].data.born;
+    const responseTwo = responses[1].data.region;
+    const responseThree = responses[2].data.coatOfArms;
+    const responseFour = responses[3].data.seats[2];
+    const responseFive = responses[4].data.aliases[1];
+    const responseSix = responses[5].data.founder;
+    const responseSeven = responses[6].data.books;
+    this.setState({data: [...responses]});
+    console.log(...responses);
+  }))
+  .catch(error => {
+    console.log(error);
+  });
+
+}
+
+  render() {
+   
       return(
           <div>
-
+            <p>{}</p>
           </div>
     );
   }
